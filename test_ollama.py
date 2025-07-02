4d3820d5144acdae23b6bd6f375aee6553d2b15e
Rafael Papa <142633852+RafaelP287@users.noreply.github.com>
2025-07-02 15:24:14 -0700

    Completed Lab 01: Offline LLM Test

--- test_ollama.py
+++ test_ollama.py
@@ -1,11 +1,17 @@
 import ollama 
 
-try:
-    response = ollama.chat(
-        model='phi:2.7b',
-        messages=[{'role':'user','content':'Explain the CIA Triad in cybersecurity.'}]
-    )
-    print(response['message']['content'])
-except Exception as e:
-    print(f"An error occured: {e}")
-    print("Please ensure the Ollama application is running.")
+def main():
+    print('=== Agentic AI LabOffline LLM Test ===')
+    user_input = input('Enter your question for the LLM: ')
+    try:
+        response = ollama.chat(
+            model='phi:2.7b',
+            messages=[{'role':'user','content':user_input}]
+        )
+        print(response['message']['content'])
+    except Exception as e:
+        print(f"An error occured: {e}")
+        print("Please ensure the Ollama application is running.")
+
+if __name__ == '__main__':
+    main()
